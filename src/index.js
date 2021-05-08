ac23c071266ce414a75b3152db01af0de144ef68
Jonathon Wilks <jonathondwilks@icloud.com>
2021-05-08 11:40:47 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+console.log(data);
+// We create individual components here
+
+
+
+
+
+
+
+
+
+
+
+
+
+// js cheat sheet
+
+// this finds an element in the html - it is the 'bridge' - its stored in a variable
+// const dogListEl = document.querySelector(".dogs-list")
+
+// this creates an element within JS (div / main / li / ul)
+//const liEl = document.createElement ('li')
+
+// this sets an attribute - in this case a class
+//liEl.setAttribute("class", "dogs-list__button")
+
+// this changes the text within an element
+//liEl.innerText = "Mr Bonkers"
+
+// this appends leEl to dogListEl (nests it)
+// dogListEl.append(liEl)
+
+// this addEventListener in the below adds a click and then function
+// liEl.addEventListener("click", function(){
+   // console.log(dog)
+
+// this set attribure is syntax for images
+//   imgEl.setAttribute("src", 
+//"https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_1.jpg")
